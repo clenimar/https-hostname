b015587ff847384945e70a6f4816a7a38acbc298
Clenimar Souza <clenimar.filemon@gmail.com>
2019-08-06 11:12:47 -0300

    disclaimer and stuff

--- src/app.py
+++ src/app.py
@@ -6,19 +6,26 @@ import ssl
 
 
 class HTTPHostnameHandler(BaseHTTPRequestHandler):
-
-
     def do_GET(self):
+        """return APP_SERVICE_TYPE env variable and hostname.
+        
+        APP_SERVICE_TYPE allows you to differentiate between "classes"
+        of services, which may be useful. if not set, it defaults
+        to... guess what... "default".
+        """
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'type: %s\nhostname: %s' % (os.getenv('APP_SERVICE_TYPE', 'A (default)').encode(),
+        self.wfile.write(b'type: %s\nhostname: %s' % (os.getenv('APP_SERVICE_TYPE', 'default').encode(),
                                                       socket.gethostname().encode()))
 
 
 httpd = HTTPServer(('0.0.0.0', 4443), HTTPHostnameHandler)
 
 
-httpd.socket = ssl.wrap_socket(httpd.socket, 
+# NOTE: encrypted certs will prompt for a passphrase every time
+# the service is started. to avoid this, we're using unencrypted
+# certs. do not use this setup in production.
+httpd.socket = ssl.wrap_socket(httpd.socket,
                                keyfile="/etc/app/key-u.pem",
                                certfile="/etc/app/cert.pem",
                                server_side=True)
